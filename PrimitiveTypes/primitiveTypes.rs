21cb0ab0f4a95d620655931a8102cf109a1a4dba
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-05-02 20:48:10 +0100

    premitive types started

---
+++ PrimitiveTypes/primitiveTypes.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+    let x = true;
+
+    let y:bool = false;
+
+    let xx = 'x';
+
+    //Unlike some other languages, this means that Rust’s char is not a single byte, but four.
+    let two_hearts = '💕';
+
+    let a = 10; // x has type i32
+
+    let b = 1.0; // y has type f64
+
+}
